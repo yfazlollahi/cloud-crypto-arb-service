45ddee8af391a2988707809043570ddaee19dd08
Yeganeh Fazlollahi <y.fazlollahi@aut.ac.ir>
2025-10-17 23:43:37 +0330

    connected to liara database

--- app/database.py
+++ app/database.py
@@ -5,13 +5,15 @@ DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
+DB_PORT = os.getenv("DB_PORT", 5432)
 
 def get_connection():
     return psycopg2.connect(
         host=DB_HOST,
         database=DB_NAME,
         user=DB_USER,
-        password=DB_PASS
+        password=DB_PASS,
+        port = DB_PORT
     )
 
 def init_db():
